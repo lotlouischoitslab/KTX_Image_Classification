cfbeca2dc0d1d863fa65bb962693925440d8ae05
lotlouischoitslab <louis.s.cho@gmail.com>
2023-05-20 21:04:13 -0500

    add

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ model = load_model('model.h5')
 model.make_predict_function()
 
 def predict_label(img_path):
-	i = image.load_img(img_path, target_size=(100,100))
+	i = image.load_img(img_path, target_size=(180,180))
 	i = image.img_to_array(i)/255.0
-	i = i.reshape(1, 100,100,3)
-	p = model.predict_classes(i)
+	i = i.reshape(1, 180,180,3)
+	p = model.predict(i)
 	return dic[p[0]]
 
 # routes
